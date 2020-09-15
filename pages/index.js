c9f87f4f661bf3083a018bc7e935abb0ed47e79c
Brendan Quinn <bquinn@sensei.com>
2020-09-15 06:36:07 -0700

    🎨

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
-import SmallCard from '../components/SmallCard';
-import { projectIcons } from '../components/Icons';
+import SmallCard from "../components/SmallCard";
+import { projectIcons } from "../components/Icons";
 
-import { projects } from '../utils/projectsData';
+import { projects } from "../utils/projectsData";
 
 const Home = () => (
   <div className="home">
-    <h1>What Can I Deploy to Static Apps?</h1>
+    <h1>What Can I Deploy to Static Apps? (🎨)</h1>
     <div className="card-grid">
       {projects.map((project) => {
         const Icon = projectIcons[project.id];
-        return <SmallCard key={project.id} Icon={Icon} title={project.name} slug={project.slug} />;
+        return (
+          <SmallCard
+            key={project.id}
+            Icon={Icon}
+            title={project.name}
+            slug={project.slug}
+          />
+        );
       })}
     </div>
   </div>
